f2e473e10a4404f938193f42e1e20404c1c04df0
Nicocube <nicolas.lochet@gmail.com>
2012-06-08 19:07:04 +0200

    adding context to json

--- bin/llama-script.js
+++ bin/llama-script.js
@@ -10,101 +10,104 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-require('knit').config(function (bind) {
-	bind('llama').to(require(__dirname + '/../lib/llama.js'))
-}).inject(function (fs, llama, str_to_obj) {	
-	var 
-		default_options_location = __dirname + '/../config.json',
-		local_value_location = './config.json'
+require('knit').inject(function (wool,knit) {
+	knit.config(function (bind) {
+		bind('llama').to(require(__dirname + '/../lib/llama.js'))
+		bind('str_to_obj').to(wool.str_to_obj).is('builder')
+	}).inject(function (fs, llama, str_to_obj) {
 		
-	var options = {
-		folder: '.',
-		static : ["/favicon.ico","/img", "/css"],
-		PORT: 8000
-	}
+		var default_options_location = __dirname + '/../config.json',
+			local_value_location = './config.json'
+			
+		var options = {
+			folder: '.',
+			static : ["/favicon.ico","/img", "/css"],
+			PORT: 8000
+		}
 
-	var usage = "Usage: llama [options]\n" +
-				"Options:\n\n" +
-				"  -c, --config FILE     the path to a JSON file with options (convention is to search for config.json in folder)\n" +
-				"  -f, --folder Folder   the path to the folder to run llama in\n" +
-				"  -p, --port PORT       the PORT number to run this llama with\n" +
-				"  -h, --help        display this help and exit\n" +
-				"  -v, --version     output version information and exit";
+		var usage = "Usage: llama [options]\n" +
+					"Options:\n\n" +
+					"  -c, --config FILE     the path to a JSON file with options (convention is to search for config.json in folder)\n" +
+					"  -f, --folder Folder   the path to the folder to run llama in\n" +
+					"  -p, --port PORT       the PORT number to run this llama with\n" +
+					"  -h, --help        display this help and exit\n" +
+					"  -v, --version     output version information and exit";
 
 
-	function loadConfig(file, noFault, noMessage) {
-		try {
-			var content = fs.readFileSync(file, 'utf8')
-			var update = str_to_obj(content)
-			for (u in update) {
-				if (typeof u === 'string') {
-					options[u] = update[u]
+		function loadConfig(file, noFault, noMessage) {
+			try {
+				var content = fs.readFileSync(file, 'utf8')
+				var update = str_to_obj(content)
+				for (u in update) {
+					if (typeof u === 'string') {
+						options[u] = update[u]
+					}
 				}
+			} catch(e) {
+				if (noFault) {
+					if (!noMessage) console.warn(e)
+				} else
+					throw e
 			}
-		} catch(e) {
-			if (noFault) {
-				if (!noMessage) console.warn(e)
-			} else
-				throw e
 		}
-	}
 
-	loadConfig(default_options_location,true,true)
+		loadConfig(default_options_location,true,true)
 
 
-	var useLocalConfig = true,
-		hasConfig=false,
-		hasFolder = false,
-		hasPort = false
+		var useLocalConfig = true,
+			hasConfig=false,
+			hasFolder = false,
+			hasPort = false
 
-	process.argv.slice(2).forEach(function (arg) {
-		switch (arg) {
-			case '-h':
-			case '--help':
-				console.log(usage)
-				process.exit(0)
-				return;
-			case '-v':
-			case '--version':
-				var content = fs.readFileSync(__dirname + '/../package.json', 'utf8')
-				var pkg = JSON.parse(content)
-				console.log(pkg.version)
-				process.exit(0)
-				return;
-			case '-c':
-			case '--config':
-				hasConfig=true
-				return;
-			case '-p':
-			case '--port':
-				hasPort=true
-				return;
-			case '-f':
-			case '--folder':
-				hasFolder=true
-				return;
-			default:
-				if (hasConfig) {
-					loadConfig(arg)
-					hasConfig=false
-					useLocalConfig=false
-				}
-				if (hasFolder) {
-					options.folder = arg
-					hasFolder=false
-				}
-				if (hasPort) {
-					options.PORT = arg
-					hasPort = false
-				}
-				return;
-		}
+		process.argv.slice(2).forEach(function (arg) {
+			switch (arg) {
+				case '-h':
+				case '--help':
+					console.log(usage)
+					process.exit(0)
+					return;
+				case '-v':
+				case '--version':
+					var content = fs.readFileSync(__dirname + '/../package.json', 'utf8')
+					var pkg = JSON.parse(content)
+					console.log(pkg.version)
+					process.exit(0)
+					return;
+				case '-c':
+				case '--config':
+					hasConfig=true
+					return;
+				case '-p':
+				case '--port':
+					hasPort=true
+					return;
+				case '-f':
+				case '--folder':
+					hasFolder=true
+					return;
+				default:
+					if (hasConfig) {
+						loadConfig(arg)
+						hasConfig=false
+						useLocalConfig=false
+					}
+					if (hasFolder) {
+						options.folder = arg
+						hasFolder=false
+					}
+					if (hasPort) {
+						options.PORT = arg
+						hasPort = false
+					}
+					return;
+			}
+
+		})
+
+		if (useLocalConfig)
+			loadConfig(local_value_location,true,true)
+
+		llama(options)
 
 	})
-
-	if (useLocalConfig)
-		loadConfig(local_value_location,true,true)
-
-	llama(options)
-
 })
