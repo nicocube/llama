b21610d1a6a2181241c5d072e8826c9d023585dd
Nicocube <nicolas.lochet@gmail.com>
2015-06-29 12:09:40 +0200

    listener impl coded, first test OK

--- spec/llama.spec.js
+++ spec/llama.spec.js
@@ -11,59 +11,64 @@
 
 var t = require( __dirname + '/../lib/llama.js')(require('mlf'))
 
-describe("template testing", function() {
+describe("template", function() {
     "use strict"
-    
-    it("events", function() {
-		var
-		$name = t.$("Plop"),
-		$arr = t.$([1,2,3]),
-		template = t.div({$:'main', _:'content'},
-			t.h1("Hello ", $name(), "!"),
-			t.ul($arr.each(
-				t.li('plip:', $arr._v())
-			))
-		)
 
-		var res = template.render()
-		
-		expect(res).toEqual('<div id="main" class="content"><h1 id="49u">Hello Plop!</h1><ul id="7iv"><li id="arw">plip:1</li><li id="e0x">plip:2</li><li id="h9y">plip:3</li></ul></div>')
+    describe("basic testing", function() {
+        var
+        $name = t.$("Plop"),
+        $arr = t.$([1,2,3]),
+        template = t.div({$:'main', _:'content'},
+            t.h1("Hello ", $name(), "!"),
+            t.ul($arr.each(
+                t.li('plip:', $arr._v())
+            ))
+        )
+        
+        it("simple Rendering", function() {
+            var res = template.render()
+            expect(res).toEqual('<div id="main" class="content"><h1 id="49u">Hello Plop!</h1><ul id="7iv"><li id="arw">plip:1</li><li id="e0x">plip:2</li><li id="h9y">plip:3</li></ul></div>')
+        })
+        
+        it("event on simple var", function() {
+            var cbCalled = false
+            template.listen(function(id, content) {
+                cbCalled = true
+                //console.log('id:',id)
+                //console.log('content:', content)
+                expect(id).toEqual('49u')
+                expect(content).toEqual('Hello Plouic!')
+            })
 
-		var cbCalled = false
-		template.listen(function(id, content) {
-			cbCalled = true
-			console.log('id:',id)
-			console.log('content:', content)
-			expect(id).toEqual('49u')
-			expect(content).toEqual('Hello Plouic!')
-		})
-
-		$name('plouic')
-		//expect(cbCalled).toEqual(true)
+            $name('Plouic')
+            expect(cbCalled).toEqual(true, "Listener was not called")
+        })
 	})
     
-    xit("events with indentation", function() {
-		var
-		$name = t.$("Plop"),
-		$arr = t.$([1,2,3]),
-		template = t.div({$:'main', _:'content'},
-			t.h1("Hello ", $name(), "!"),
-			t.ul($arr.each(
-				t.li('plip:', t._())
-			))
-		)
+    xdescribe("basic testing", function() {
+        xit("events with indentation", function() {
+            var
+            $name = t.$("Plop"),
+            $arr = t.$([1,2,3]),
+            template = t.div({$:'main', _:'content'},
+                t.h1("Hello ", $name(), "!"),
+                t.ul($arr.each(
+                    t.li('plip:', t._())
+                ))
+            )
 
-		var res = template.render({indent: 2})
-		
-		expect(res).toEqual('<div id="main" class="content">\n  <h1 id="main-h1-a">Hello Plop!</h1>\n  <ul id="main-ul-a">\n    <li id="main-ul-a-li-a">plip:1</li>\n    <li id="main-ul-a-li-b">plip:2</li>\n    <li id="main-ul-a-li-c">plip:3</li>\n  </ul>\n</div>')
+            var res = template.render({indent: 2})
+            
+            expect(res).toEqual('<div id="main" class="content">\n  <h1 id="main-h1-a">Hello Plop!</h1>\n  <ul id="main-ul-a">\n    <li id="main-ul-a-li-a">plip:1</li>\n    <li id="main-ul-a-li-b">plip:2</li>\n    <li id="main-ul-a-li-c">plip:3</li>\n  </ul>\n</div>')
 
-		template.listen(function(id, content) {
-			console.log('id:',id)
-			console.log('content:', content)
-		})
+            template.listen(function(id, content) {
+                console.log('id:',id)
+                console.log('content:', content)
+            })
 
-		$name('plouic')
-		// console.log -> id: main-h1-a
-		// console.log -> content: Hello Plouic!
+            $name('plouic')
+            // console.log -> id: main-h1-a
+            // console.log -> content: Hello Plouic!
+        })
 	})
 })
