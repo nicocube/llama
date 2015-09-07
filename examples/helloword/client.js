9196d5701e84856a333e02bd9c709c1c47d2f735
Nicocube <nicolas.lochet@gmail.com>
2015-09-07 16:35:35 +0200

    update helloworld example to llama 0.2.5

--- examples/helloword/client.js
+++ examples/helloword/client.js
@@ -1,9 +1,9 @@
 /*
  * Copyright 2015 Nicolas Lochet Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy of the License at
- *      
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is
  * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License.
@@ -17,16 +17,15 @@ var t = require('llama')()
 
 //console.log(document.getElementsByTagName('html').item(0).id)
 
-page.render()
 page.listen(function(id, html) {
-	console.log('id', id, html)
-	document.getElementById(id).innerHTML = html
+    console.log('id', id, html)
+    document.getElementById(id).innerHTML = html
 })
 
 function onevent() {
-	console.log('event', data.value)
-	name(data.value)
-	history.pushState(null, null, '/'+data.value)
+    console.log('event', data.value)
+    name(data.value)
+    history.pushState(null, null, '/'+data.value)
 }
 
 data.addEventListener('keyup', onevent)
--- examples/helloword/server.js
+++ examples/helloword/server.js
@@ -1,9 +1,9 @@
 /*
  * Copyright 2015 Nicolas Lochet Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License. You may obtain a copy of the License at
- *      
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is
  * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License.
@@ -23,17 +23,17 @@ app.use(favicon(__dirname + '/public/favicon.ico'))
 app.use(morgan('dev'))
 app.use(router)
 
-router.get('/client.js', browserify('./client.js'));
+router.get('/client.js', browserify('./client.js'))
 
 router.get('/', function (req, res) {
-	var name = t.$('World')
+    var name = t.$('World')
     res.send(main(name).render(options))
 })
 
 router.get('/:name', function (req, res) {
-	var name = t.$(req.params.name)
+    var name = t.$(req.params.name)
     res.send(main(name).render(options))
 })
 
 app.listen(3000)
-console.log('Server is running.\nOpen http://localhost:3000/')
+process.stdout.write('Server is running.\nOpen http://localhost:3000/\n')
